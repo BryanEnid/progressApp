42b47bf4f7097d636ee53ea0865f77120b76e80d
Bryan Enid <bryanenid@Bryans-iMac.local>
2018-05-20 01:43:36 -0400

    Added logo and new theme

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-    
+    <img src="./assets/logo.png" alt="progress" class="logo">
 
     <div id="items">
       <div id="total">
       <p>Total:</p>
-      <p>{{ total() }}</p>
+      <p>{{ total() }} /monthly</p>
     </div>
 
     <!-- Done** Boxes of expenses located in Server -->
@@ -21,27 +21,19 @@
       <p class="expense">${{expense.amount}}</p>
       
     </div>
-    <button v-on:click.prevent="addexpense = false" v-show="addexpense" class="button">Add Expense</button>
+    <button v-on:click.prevent="addexpense = false" v-show="addexpense" class="button">&#x2795;</button>
     </div>
     
     <form id="add-blog" v-show="!addexpense">
       <h2>Add Expense</h2>
-      <input type="text" placeholder="Expense name or Company" ref="input" v-model="newExpense.name" maxlength="50"><br>
-      <input type="text" placeholder="Type" ref="input" v-model="newExpense.type" maxlength="10"><br>
-      <input type="number" placeholder="Amount" ref="amount" v-model="newExpense.amount"><br>
-      <input type="date" placeholder="Due Date" ref="due" v-model="newExpense.due" v-bind:min="actualDate()"><br>
+      Name:<input type="text" placeholder="Expense name or Company" ref="input" v-model="newExpense.name" maxlength="50">
+      Type:<input type="text" placeholder="Monthly recurring, loan, credit, etc..." ref="input" v-model="newExpense.type" maxlength="20">
+      Amount: <input type="number" placeholder="$0.00" ref="amount" v-model="newExpense.amount">
+      Date: <input type="date" placeholder="Due Date" ref="due" v-model="newExpense.due" v-bind:min="actualDate()">
       <button v-on:click.prevent="addexpense = true; reset()" class="buttonCancel">Cancel</button>
       <span v-if="newExpense.name != '' && newExpense.type != '' && newExpense.due != '' && newExpense.amount != ''">
         <button v-on:click.prevent="add()" class="buttonSubmit">Submit</button>
       </span>
-      <div id="preview" v-if=" newExpense.name != '' || newExpense.type != '' ||  newExpense.due != '' || newExpense.amount != ''">
-        <h2>Preview Form</h2>
-        <p>Expense: {{ newExpense.name }}</p><br>
-        <p>Type: {{ newExpense.type }}</p><br>
-        <p>Amount: <span v-if="newExpense.amount != ''">$</span>{{ newExpense.amount }}</p><br>
-        <p>Due Date: {{ newExpense.due }}</p><br>
-      </div>
-      <br>
     </form>
     
     
@@ -51,39 +43,39 @@
       <fulfilling-bouncing-circle-spinner
       :animation-duration="4000"
       :size="60"
-      :color="'#4ed85b'"
+      :color="'#00a99e'"
       id="loader"
       v-if="loader"
       />
     
 
-    <center><a href="">See this app repository</a> </center>
+    <center><a href="https://github.com/BryanEnid/progressApp">See this app repository</a> </center>
     
   </div>
 </template>
 
 <script>
-import Firebase,{ functions } from "firebase";
-import Toastr from 'toastr';
-import { FulfillingBouncingCircleSpinner } from 'epic-spinners';
+import Firebase, { functions } from "firebase";
+import Toastr from "toastr";
+import { FulfillingBouncingCircleSpinner } from "epic-spinners";
 
 Toastr.options = {
-  "closeButton": true,
-  "debug": false,
-  "newestOnTop": false,
-  "progressBar": true,
-  "positionClass": "toast-bottom-right",
-  "preventDuplicates": true,
-  "onclick": null,
-  "showDuration": "300",
-  "hideDuration": "1000",
-  "timeOut": "5000",
-  "extendedTimeOut": "1000",
-  "showEasing": "swing",
-  "hideEasing": "linear",
-  "showMethod": "fadeIn",
-  "hideMethod": "fadeOut"
-}
+  closeButton: true,
+  debug: false,
+  newestOnTop: false,
+  progressBar: true,
+  positionClass: "toast-bottom-right",
+  preventDuplicates: true,
+  onclick: null,
+  showDuration: "300",
+  hideDuration: "1000",
+  timeOut: "5000",
+  extendedTimeOut: "1000",
+  showEasing: "swing",
+  hideEasing: "linear",
+  showMethod: "fadeIn",
+  hideMethod: "fadeOut"
+};
 
 let config = {
   apiKey: "AIzaSyDDILZWKSq_SwyEilYP3t07lNu3XVaA7YM",
@@ -94,11 +86,9 @@ let config = {
   messagingSenderId: "759681807256"
 };
 
-
-
 let app = Firebase.initializeApp(config);
 let db = app.database();
-let expensesRef = db.ref("expenses")
+let expensesRef = db.ref("expenses");
 
 export default {
   components: {
@@ -109,57 +99,57 @@ export default {
       newExpense: {
         name: "",
         type: "",
-        amount: '',
+        amount: "",
         due: ""
       },
       edit: false,
       addexpense: true,
       loader: true,
-      actualDate(){
+      actualDate() {
         let today = new Date();
         let dd = "0" + today.getDate();
         let mm = "0" + (today.getMonth() + 1);
         let yy = today.getFullYear();
-        let dateString =  yy.toString() + "-" + mm.slice(-2).toString() + "-01"
-        return dateString
+        let dateString = yy.toString() + "-" + mm.slice(-2).toString() + "-01";
+        return dateString;
       }
     };
   },
   firebase: {
     expenses: expensesRef
   },
-  mounted(){
+  mounted() {
     db.ref("expenses").once("value", snap => {
-      window.setTimeout(this.hideLoader, 3300)
+      window.setTimeout(this.hideLoader, 1000);
       console.log("All files load...");
-    })
+    });
   },
   methods: {
     add() {
-      Toastr.remove()
-      let error = false
-      let numbers = ["0","1","2","3","4","5","6","7","8","9"]
-      if (this.newExpense.amount < 1){
-        Toastr.error('Your amount should be more than 0');
-        error = true ;
+      Toastr.remove();
+      let error = false;
+      let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+      if (this.newExpense.amount < 1) {
+        Toastr.error("Your amount should be more than 0");
+        error = true;
       }
-      for (let letter of this.newExpense.name){
+      for (let letter of this.newExpense.name) {
         if (numbers.includes(letter)) {
           Toastr.error("The name of the company should'nt contain numbers");
-          error = true ;
-          break;
-        }
-      } 
-      for (let letter of this.newExpense.type){
-        if (numbers.includes(letter)) {
-          Toastr.error("The type should'nt contain numbers");
-          error = true ;
+          error = true;
           break;
         }
       }
-      if (error == false){
+      for (let letter of this.newExpense.type) {
+        if (numbers.includes(letter)) {
+          Toastr.error("The type should'nt contain numbers");
+          error = true;
+          break;
+        }
+      }
+      if (error == false) {
         expensesRef.push(this.newExpense);
-        Toastr.success('You added a new expense...');
+        Toastr.success("You added a new expense...");
         this.newExpense.name = "";
         this.newExpense.type = "";
         this.newExpense.amount = "";
@@ -168,34 +158,32 @@ export default {
       }
     },
     remove(expense) {
-      expensesRef.child(expense['.key']).remove()
-      Toastr.success('You delete ' + '"'+ expense.name +'"')      
+      expensesRef.child(expense[".key"]).remove();
+      Toastr.success("You delete " + '"' + expense.name + '"');
     },
-    editExpense(expense){
-      expensesRef.child(expense['.key']).update()
-      Toastr.success('Edit succesfully')
+    editExpense(expense) {
+      expensesRef.child(expense[".key"]).update();
+      Toastr.success("Edit succesfully");
     },
-    total(){
-        let total = 0;
-        for (let expense in this.expenses){
-          let number = parseFloat(this.expenses[expense].amount)
-          total = total + number
-        }
-        return total.toFixed(2)
+    total() {
+      let total = 0;
+      for (let expense in this.expenses) {
+        let number = parseFloat(this.expenses[expense].amount);
+        total = total + number;
+      }
+      return total.toFixed(2);
     },
-    reset(){
+    reset() {
       this.newExpense.name = "";
       this.newExpense.type = "";
       this.newExpense.amount = "";
       this.newExpense.due = "";
     },
-    hideLoader(){
-      this.loader = false
+    hideLoader() {
+      this.loader = false;
     }
   }
 };
-
-
 </script>
 
 
@@ -207,17 +195,37 @@ export default {
 
 
 <style>
+.logo {
+  display: block;
+  height: 100px;
+  width: 100px;
+  margin: 30px auto;
+}
+
+center a {
+  font-weight: bold;
+  color: #00a99e;
+}
+
+body {
+  background: #222;
+}
+
+center {
+  margin-top: 100px;
+}
+
 .backgroundW {
   top: 0;
   left: 0;
-  background:white;
+  background: #222;
   position: fixed;
   width: 100%;
   height: 100%;
 }
 
 #loader {
-  position:fixed;
+  position: fixed;
   left: 50%;
   top: 45%;
   margin-top: -30px;
@@ -228,12 +236,12 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: white;
   padding: 8px;
 }
 
 body::-webkit-scrollbar {
-  width: 0 !important
+  width: 0 !important;
 }
 
 p {
@@ -246,11 +254,16 @@ label {
 }
 
 /* Buttons */
-button {
-  background-color: transparent;
+.button {
   border-radius: 3px;
   font-size: 15px;
   outline: none;
+  border: 1px solid #999;
+  box-shadow: 0px 0px 10px #00a99e;
+  max-width: 500px;
+  width: 100%;
+  height: 50px;
+  margin-top: 40px;
 }
 button:active,
 button:checked {
@@ -259,10 +272,13 @@ button:checked {
 
 .buttonCancel {
   border: 1px solid #f76c6c;
+  box-shadow: 0px 0px 10px #f76c6c;
+  margin-right: 10px;
 }
 
 .buttonSubmit {
   border: 1px solid #4ed85b;
+  box-shadow: 0px 0px 10px #00a99e;
 }
 
 .deleteButton {
@@ -270,7 +286,9 @@ button:checked {
   border: none;
   float: right;
   height: auto;
-  margin-top: 20px;
+  margin-top: 7px;
+  color: white;
+  background: transparent;
 }
 .editButton {
   width: auto;
@@ -300,27 +318,31 @@ button:checked {
 }
 
 .box {
-  border: 1px solid #888;
+  border: 1px solid #00a99e;
   margin: 10px 0;
   border-radius: 3px;
   padding: 10px 30px 15px 30px;
-  color: #333;
+  color: white;
   position: relative;
+  background: #444;
 }
+
+.box h2 {
+  color: white;
+}
+
 .box p {
   font-size: 18px;
   margin: 0px;
 }
 
-.box h2 {
-  margin: 20px 0 110px 0;
-}
-
 #total {
+  border: 1px solid #00847b;
   border-radius: 3px;
   max-width: 500px;
-  background: #4ed85b;
+  background: #00a99e;
   color: white;
+  margin: 30px 0;
 }
 
 #total p {
@@ -338,10 +360,6 @@ button:checked {
 }
 
 /* Form */
-
-#preview {
-  margin-top: 35px;
-}
 
 #preview p {
   display: inline;
@@ -361,13 +379,16 @@ textarea {
   padding: 8px;
   border: 1px solid #888;
   border-radius: 3px;
+  margin-bottom: 10px;
 }
 
 input[type="date"] {
   display: block;
-  width: 50%;
+  height: 40px;
+  width: 30%;
   padding: 8px;
   border: 1px solid #888;
   border-radius: 3px;
+  margin-bottom: 30px;
 }
 </style>
