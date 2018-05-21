f44d917dea6ec5da38ee8446b1787421e75ec241
Bryan Enid <bryanenid@Bryans-iMac.local>
2018-05-20 21:08:33 -0400

    Added Bootstrap Vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueFire from 'vuefire';
+import BootstrapVue from 'bootstrap-vue'
 
+Vue.use(BootstrapVue);
 Vue.use(VueFire);
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
   el: '#app',
